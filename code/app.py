0ce29eaa3ee3a49e37bee4cfe116b26e69117e18
RuchiPatil <ruchipatil@outlook.com>
2022-06-05 16:01:05 -0400

    server image get

--- code/app.py
+++ code/app.py
@@ -1,28 +1,40 @@
-from flask import Flask, jsonify, request, render_template
-from flask_restful import Resource, Api
+from flask import Flask, request, Response, send_file, send_from_directory
+import jsonpickle
+import numpy as np
+import cv2
 
+# Initialize the Flask application
 app = Flask(__name__)
-api = Api(app)
-
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
-
-api.add_resource(Student, '/student/<string:name>')
-#blah
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# route http posts to this method
+@app.route('/api/test', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite('download2.jpg', img)
 
-@app.route('/student', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-    'name' : request_data['name']
-    }
-    store.append(new_student)
-    return jsonify(new_student)
+    # do some fancy processing here....
 
-app.run(port=5000)
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+#UPON REQUEST sending image to Receiver (and then deleteing image 1 minute after sending)
+@app.route("/get_image")
+def get_image():
+
+    return send_from_directory('./', 'get.jpg', as_attachment=True)
+    #return send_file(img, mimetype='image/gif', as_attachment=True)
+#UPON REQUEST receiving image from Sender
+
+
+# start flask app
+app.run(host="0.0.0.0", port=5000)
