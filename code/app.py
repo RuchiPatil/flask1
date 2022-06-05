0a47033b33a3307d6c279436ff3a99b230d7895a
RuchiPatil <ruchipatil@outlook.com>
2022-06-05 15:07:31 -0400

    server image get

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -10,4 +10,19 @@ class Student(Resource):
 
 api.add_resource(Student, '/student/<string:name>')
 #blah
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/student', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+    'name' : request_data['name']
+    }
+    store.append(new_student)
+    return jsonify(new_student)
+
 app.run(port=5000)
